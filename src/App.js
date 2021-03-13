b9dc7a3b2dabb405bd36a1f428e587e177409180
H Nazmul Hassan <71649134+HNazmul@users.noreply.github.com>
2021-03-13 17:35:53 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "./App.css";
+import { firebaseConfig } from "./firebase.config";
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // const [provider, setProvider] = useState(null);
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    const [signedInUser, setSignedInUser] = useState({
+        isLogin: false,
+        name: "",
+        photo: "",
+        email: "",
+        credential: {},
+        token: {},
+    });
+
+    /**
+     * Google sign in funtion
+     * will sign in with google apies
+     */
+    const handleSignIn = () => {
+
+        firebase
+            .auth()
+            .signInWithPopup(provider)
+            .then((result) => {
+                console.log(result);
+
+                const credential = result.credential;
+                const token = credential.accessToken;
+                const { displayName, email, photoURL } = result.user;
+
+                setSignedInUser({
+                    isLogin: true,
+                    name: displayName,
+                    email: email,
+                    credential: credential,
+                    token: token,
+                    photo: photoURL,
+                })
+            }).catch((err) => {
+              console.log(err);
+            })
+    }
+
+
+    const handleSignOut = () => {
+        firebase
+        .auth().signOut()
+        .then(() => {
+            setSignedInUser({
+                isLogin: false,
+                name: '',
+                email: '',
+                credential: '',
+                token: '',
+                photo: '',
+            });
+        }).catch((err) => {
+            console.log(err);
+        })
+    }
+
+    return (
+        <>
+            <div className="sign-in-area">
+                <div>
+                    {signedInUser.isLogin ? (
+                        <button onClick={handleSignOut} className="btn w-100 btn-light"> Sign out</button>
+                    ) : (
+                        <button onClick={handleSignIn} className="btn w-100 btn-light">
+                            Sing in
+                        </button>
+                    )}
+                </div>
+                {signedInUser.isLogin ? (
+                    <div className="card user-details-card mb-3">
+                        <img src={signedInUser.photo} className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Welcome to, {signedInUser.name}</h5>
+                            <p className="card-text">Email: {signedInUser.email}.</p>
+                            <p className="card-text">
+                                <small className="text-muted">Last updated 3 mins ago</small>
+                            </p>
+                        </div>
+                    </div>
+                ) : (
+                    <div className="user-details-card">
+                        <h1>Please login For Continue</h1>
+                    </div>
+                )}
+            </div>
+        </>
+    );
 }
 
 export default App;
---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCPO3Gq9W4V1NC3JQK6nah85Pcp7dxy5Xk",
+    authDomain: "ema-john-d26de.firebaseapp.com",
+    projectId: "ema-john-d26de",
+    storageBucket: "ema-john-d26de.appspot.com",
+    messagingSenderId: "828481032123",
+    appId: "1:828481032123:web:8995cd5cc1ba3f8a6dbe9e",
+};
+
+export {firebaseConfig}
